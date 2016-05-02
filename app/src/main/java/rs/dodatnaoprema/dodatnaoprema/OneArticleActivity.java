b3c14809995f573cda26f1b59388f5267cb019ae
Mirna <mirnatasovic@gmail.com>
2016-05-03 01:48:08 +0200

    one_product

--- app/src/main/java/rs/dodatnaoprema/dodatnaoprema/OneArticleActivity.java
+++ app/src/main/java/rs/dodatnaoprema/dodatnaoprema/OneArticleActivity.java
@@ -1,11 +1,7 @@
 package rs.dodatnaoprema.dodatnaoprema;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.widget.TextView;
 
 import com.android.volley.toolbox.ImageLoader;
@@ -18,9 +14,14 @@ import rs.dodatnaoprema.dodatnaoprema.network.VolleySingleton;
 public class OneArticleActivity extends AppCompatActivity {
 
     private NetworkImageView mImageView;
-    private TextView mTextViewProizvodjac;
+    private TextView mTextViewBrendName;
     private TextView mTextViewProductName;
     private TextView mTextViewPrice;
+    private TextView mTextViewAboutPrice;
+
+    private TextView mTextViewStars;
+    private TextView mTextViewYesNo;
+    private TextView mTextViewMin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,17 +29,33 @@ public class OneArticleActivity extends AppCompatActivity {
         setContentView(R.layout.activity_one_article);
 
         mImageView = (NetworkImageView)  findViewById(R.id.img_one_product);
-        mTextViewProizvodjac = (TextView) findViewById(R.id.textView_proizvodjac);
+        mTextViewBrendName = (TextView) findViewById(R.id.textView_brend_name);
         mTextViewProductName = (TextView) findViewById(R.id.textView_naziv);
         mTextViewPrice= (TextView) findViewById(R.id.textView_cena);
+        mTextViewAboutPrice = (TextView) findViewById(R.id.textView_about_price);
+
+        mTextViewStars = (TextView) findViewById(R.id.textView_stars);
+        mTextViewYesNo = (TextView) findViewById(R.id.textView_yes_no);
+        mTextViewMin = (TextView) findViewById(R.id.textView_min);
+
 
         OneArticle oneArticle= (OneArticle) getIntent().getExtras().get(AppConfig.ABOUT_PRODUCT);
         ImageLoader mImageLoader = VolleySingleton.getsInstance(this).getImageLoader();
 
         mImageView.setImageUrl(oneArticle.getArtikal().getSlike().get(0).getSrednjaSlika(), mImageLoader);
-        mTextViewProizvodjac.setText(oneArticle.getArtikal().getBrendIme());
+        mTextViewBrendName.setText(oneArticle.getArtikal().getBrendIme());
         mTextViewProductName.setText(oneArticle.getArtikal().getArtikalNaziv());
         mTextViewPrice.setText(oneArticle.getArtikal().getCenaSamoBrojFormat()+" "+oneArticle.getArtikal().getCenaPrikazExt());
+        mTextViewAboutPrice.setText("cena po: "+ oneArticle.getArtikal().getTipUnitCelo());
+
+        mTextViewStars.setText("ocena: "+ oneArticle.getArtikal().getOcenaut());
+
+        if (oneArticle.getArtikal().getStanje() == 1)
+            mTextViewYesNo.setText("ima na stanju");
+        else
+            mTextViewYesNo.setText("nema na stanju");
+
+        mTextViewMin.setText("Minimalna količina za narudžbinu: " + oneArticle.getArtikal().getMozedasekupi());
 
     }
 
